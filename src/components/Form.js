87842b795c8e3e0191e023fc62c7d1b000fd1e56
Higor Zica <higorbhe@gmail.com>
2023-01-23 11:18:46 -0300

    first commit Frontend

---
+++ src/components/Form.js
@@ -0,0 +1,131 @@
+import axios from "axios";
+import React, { useEffect, useRef } from "react";
+import styled from "styled-components";
+import { toast } from "react-toastify";
+
+const FormContainer = styled.form`
+  display: flex;
+  align-items: flex-end;
+  gap: 10px;
+  flex-wrap: wrap;
+  background-color: #fff;
+  padding: 20px;
+  box-shadow: 0px 0px 5px #ccc;
+  border-radius: 5px;
+`;
+
+const InputArea = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  width: 120px;
+  padding: 0 10px;
+  border: 1px solid #bbb;
+  border-radius: 5px;
+  height: 40px;
+`;
+
+const Label = styled.label``;
+
+const Button = styled.button`
+  padding: 10px;
+  cursor: pointer;
+  border-radius: 5px;
+  border: none;
+  background-color: #034195;
+  color: white;
+  height: 44px;
+  transition: transform ease-in;
+  &:hover{
+    background-color:#0759c8;
+    transform:scale(1.01);
+    font-weight: bold;
+  }
+`;
+
+const Form = ({ getUsers, onEdit, setOnEdit }) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    if (onEdit) {
+      const user = ref.current;
+
+      user.nome.value = onEdit.nome;
+      user.email.value = onEdit.email;
+      user.celular.value = onEdit.celular;
+      user.data_cad.value = onEdit.data_cad;
+    }
+  }, [onEdit]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const user = ref.current;
+
+    if (
+      !user.nome.value ||
+      !user.email.value ||
+      !user.celular.value ||
+      !user.data_cad.value
+    ) {
+      return toast.warn("Preencha todos os campos!");
+    }
+
+    if (onEdit) {
+      await axios
+        .put("http://localhost:8800/" + onEdit.id, {
+          nome: user.nome.value,
+          email: user.email.value,
+          celular: user.celular.value,
+          data_cad: user.data_cad.value,
+        })
+        .then(({ data }) => toast.success(data))
+        .catch(({ data }) => toast.error(data));
+    } else {
+      await axios
+        .post("http://localhost:8800", {
+          nome: user.nome.value,
+          email: user.email.value,
+          celular: user.celular.value,
+          data_cad: user.data_cad.value,
+        })
+        .then(({ data }) => toast.success(data))
+        .catch(({ data }) => toast.error(data));
+    }
+
+    user.nome.value = "";
+    user.email.value = "";
+    user.celular.value = "";
+    user.data_cad.value = "";
+
+    setOnEdit(null);
+    getUsers();
+  };
+
+  return (
+    <FormContainer ref={ref} onSubmit={handleSubmit}>
+      <InputArea>
+        <Label>Nome</Label>
+        <Input name="nome" />
+      </InputArea>
+      <InputArea>
+        <Label>E-mail</Label>
+        <Input name="email" type="email" />
+      </InputArea>
+      <InputArea>
+        <Label>Telefone</Label>
+        <Input name="celular" />
+      </InputArea>
+      <InputArea>
+        <Label>Data de Cadastro</Label>
+        <Input name="data_cad" type="date" />
+      </InputArea>
+
+      <Button type="submit">SALVAR</Button>
+    </FormContainer>
+  );
+};
+
+export default Form;
---
+++ src/styles/global.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+
+    *{
+        margin: 0;
+        padding: 0;
+    }
+
+    body{
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content:center;
+        font-family: 'Poppins', sans-serif;
+        background-color: #f2f2f2;
+        
+    }
+`;
+
+export default Global;
